f81dafb41679ccecbe2a4c3530941b320d313eb5
msksh <74917190+msksh@users.noreply.github.com>
2020-12-24 22:35:29 +0900

    reset python basic
    파이썬 기초 다시잡기

---
+++ practice.py
@@ -0,0 +1,33 @@
+'''
+station = "인천공항" 
+print(station+"행 열차가 들어오고있습니다.")
+'''
+'''
+#연산자
+print(1+1)#2
+print(3-1)#2
+print(2*5)#10
+print(6/3)#2
+print(2**3) #2^3=8
+print(5%3) #나머지구하기 2
+print(10%3) #1
+print(5//3) #1
+print(10//3) #3
+
+print( 10 > 3) #True
+print(4 >= 7)#False
+print(10 < 3) #False
+print(5 <= 5) #True
+
+print(3 == 3)#true
+print(4 == 2)#False
+print(3+4 == 7) #True
+print( 1!=3)#True
+print(not(1!=3))#False
+
+print((3>0) and (3<5)) #True
+print((3>0)&(3<5)) #True
+
+print((3>0) or (3<5)) #True
+print((3>0)|(3<5)) #True
+'''
