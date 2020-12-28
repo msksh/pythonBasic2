45d8d2efa08be9b551ffc89211e9731bc11f4669
msksh <74917190+msksh@users.noreply.github.com>
2020-12-28 23:29:01 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -728,5 +728,373 @@ print(python.count("n"))
 
 # score_file.close()
 
+#pickle
+# import pickle
+# profile_file = open("profile.pickle","wb")
+# profile = {"이름":"박병수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) #proflie에 있는 정보를 file에 저장
+# profile_file.close()
 
+# profile_file = open("profile.pickle","rb")
+# profile = pickle.load(profile_file)  #file에 있는 정보를 profile에 불러오기
+# print(profile)
+# profile_file.close()
+
+#with - close가 필요없음
+# import pickle
+
+# with open("profile.pickle", "rb")as profile_file:
+#     print(pickle.load(profile_file))
+
+#with 을 이용한 일반적인 파일생성
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+# with open ("study.txt","r", encoding="utf8") as study_file:
+#     print(study_file.read())
+
+# Quiz) 당신의 회사에서는 매주 1회 작성해야 하는 보고서가 있습니다.
+# 보고서는 항상 아래와 같은 형태로 출력되어야합니다. 
+
+# -X 주차 주간보고-
+# 부서 :
+# 이름 :
+# 업무 요약 :
+
+# 1주차부터 50주차까지의 보고서 파일을 만드는 프로그램을 작성하시오.
+# 조건 : 파일명은 '1주차.txt','2주차.txt',...와 같이 만듭니다.
+
+#with open("i 주차.txt","w", encoding="utf8") as report_file:
+
+# #내답 까비 거의다 맞았는데 ㅠㅠ 
+# for i in range(1,51):
+#     report_file = open("i 주차.txt", "w", encoding="utf=8")
+#     print("-i 주차 주간보고-", file=report_file)
+#     print("부서 : ", file=report_file)
+#     print("이름 : ", file=report_file)
+#     print("업무요약 : ", file=report_file)
+#     report_file.close()
+
+#모범답안
+# for i in range(1,51):
+#     with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+#         report_file.write("-{0} 주차 구간보고-".format(i))
+#         report_file.write("\n부서 :")
+#         report_file.write("\n이름 :")
+#         report_file.write("\n업무 요약 :")
+
+#class <- 보통 붕어빵틀이라고 비유함 틀만 있으면 무한생산가능
+
+# #마린 : 공격 유닛, 군인.총을 쓸 수 있음
+# name="마린" #유닛의 이름
+# hp = 40 #유닛의 체력
+# damage = 5 #유닛의 공격력
+
+# print("{0} 유닛이 생성되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp,damage))
+
+# #탱크 : 공격 유닛, 탱크.포를 쏠 수 있는데, 일반모드/시즈모드.
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{0} 유닛이 생성되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp,tank_damage))
+
+# def attack(nmae, location, damage):
+#     print("{0} : {1} 방향으로 적군을 공격 합니다.[공격력 {2}]".format(\
+#         name, location, damage))
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+# class Unit:
+#     def __init__(self, name, hp, damage):
+#         self.name = name
+#         self.hp = hp
+#         self.damage = damage
+#         print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+# #레이스 : 공중 유닛, 비행기.클로킹(상대에게 보이지 않음)
+# wraith1 = Unit("레이스", 80, 5)
+# print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# #마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+# wraith2 = Unit("레이스", 80, 5)
+# wraith2.clocking = True
+
+# if wraith2.clocking == True:
+#     print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+#메소드
+# #일반 유닛
+# class Unit:
+#     def __init__(self, name, hp): #damage):
+#         self.name = name
+#         self.hp = hp
+#         #self.damage = damage
+#         #print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         #print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+# #공격유닛
+# class AttackUnit(Unit): #class AttackUnit 안에 Unit를 상속받음
+#     def __init__(self, name, hp, damage):
+#         # self.name = name
+#         # self.hp = hp
+#         Unit.__init__(self, name, hp)
+#         self.damage = damage
+
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격 합니다.[공격력 {2}]"\
+#             .format(self.name, location, self.damage))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+
+# #매딕 : 의무병
+
+# #파이어뱃 : 공격 유닛, 화염방사기.
+# firebat1 = AttackUnit("파이어뱃", 50, 16)
+# firebat1.attack("5시")
+
+# #공격 2번 받는다고 가정
+# firebat1.damaged(25)
+# firebat1.damaged(25)
+
+
+#다중상속
+
+# #일반 유닛
+# class Unit:
+#     def __init__(self, name, hp): #damage):
+#         self.name = name
+#         self.hp = hp
+#         #self.damage = damage
+#         #print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         #print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+# #공격유닛
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, damage):
+#         # self.name = name
+#         # self.hp = hp
+#         Unit.__init__(self, name, hp)
+#         self.damage = damage
+
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격 합니다.[공격력 {2}]"\
+#             .format(self.name, location, self.damage))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+
+# # 드랍쉽 : 공중유닛, 수송기. 마린/파이엇뱃/탱크 등을 수송 공격불가
+# # 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+#             .format(name, location, self.flying_speed))
+
+# #공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):   #다중상속함
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, damage)
+#         Flyable.__init__(self, flying_speed)
+
+# #발키리 : 공중 공격유닛. 한번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+# valkyrie.fly(valkyrie.name, "3시")
+
+
+
+# #메소드 오버라이딩
+
+# #일반 유닛
+# class Unit:
+#     def __init__(self, name, hp, speed): #damage):
+#         self.name = name
+#         self.hp = hp
+#         self.speed = speed
+    
+#     def move(self, location):
+#         print("[지상 유닛 이동]")
+#         print("{0} : {1} 방향으로 이동합니다.[속도 {2}]".format(self.name, location, self.speed))
+#         #self.damage = damage
+#         #print("{0} 유닛이 생성 되었습니다.".format(self.name))
+#         #print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+# #공격유닛
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, speed, damage):
+#         # self.name = name
+#         # self.hp = hp
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage
+
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격 합니다.[공격력 {2}]"\
+#             .format(self.name, location, self.damage))
+
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+
+
+# # 드랍쉽 : 공중유닛, 수송기. 마린/파이엇뱃/탱크 등을 수송 공격불가
+# # 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
+
+#     def fly(self, name, location):
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+#             .format(name, location, self.flying_speed))
+
+# #공중 공격 유닛 클래스
+# class FlyableAttackUnit(AttackUnit, Flyable):   #다중상속함
+#     def __init__(self, name, hp, damage, flying_speed):
+#         AttackUnit.__init__(self, name, hp, 0, damage) #지상 스피드는 0으로처리
+#         Flyable.__init__(self, flying_speed)
+
+#     def move(self, location):
+#         print("[공중유닛이동]")
+#         self.fly(self.name, location)
+
+# #pass
+# #건물
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#       # Unit.__init__(self, name, hp , 0)
+#        super().__init__(name, hp, 0)
+#        self.location = location
+
+
+
+#스타크래프트 전반전
+
+
+#일반 유닛
+class Unit:
+    def __init__(self, name, hp, speed): #damage):
+        self.name = name
+        self.hp = hp
+        self.speed = speed
+        print("{0} 유닛이 생성되었습니다.".format(name))
+    
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다.[속도 {2}]".format(self.name, location, self.speed))
+        #self.damage = damage
+        #print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        #print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+    
+#공격유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, speed, damage):
+        # self.name = name
+        # self.hp = hp
+        Unit.__init__(self, name, hp, speed)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격 합니다.[공격력 {2}]"\
+            .format(self.name, location, self.damage))
+
+#마린
+class Marine(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self, "마린", 40, 1, 5)
+
+    #스팀팩 : 일정 시간 동안 이동 및 공격 속도를 증가, 체력10감소
+    def stimpack(self):
+        if self.hp >10:
+            slef.hp -= 10
+            print("{0} : 스팀팩을 사용합니다.(HP 10 감소)".format(self.name))
+        else:
+            print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
+
+
+#탱크
+class Tank(AttackUnit):
+    #시즈모드 : 탱크를 지상에 고정시켜, 더높은 파워로 공격 가능. 이동불가
+    seize_developed = False #시즈모드 개발여부
+
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 1, 35)
+
+    def set_seize_mode(self):
+        if Tank.seize_developed == False:
+            return
+
+        #현재 시즈모드가 아닐때 -> 시즈모드
+        if self.seize_mode == False:
+            print("{0} : 시즈모드로 전환합니다.".format(self.name))
+            self.damage *= 2
+            self.seize_mode = True
+
+
+        #현재 시즈모드일 -> 시즈모드 해제
+        else:
+            print("{0} : 시즈모드를 해제합니다.".format(self.name))
+            self.damage /= 2
+            self.seize_mode = False
+
+
+# 드랍쉽 : 공중유닛, 수송기. 마린/파이엇뱃/탱크 등을 수송 공격불가
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+
+#공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):   #다중상속함
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) #지상 스피드는 0으로처리
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중유닛이동]")
+        self.fly(self.name, location)
+
+#레이스
+class Wraith(FlyableAttackUnit):
+    def __init__(self):
+        FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
+        self.clocked = False #클로킹 모드 (해체 상태)
+
+    def clocking(self):
+        if self.clocked == True: #클로킹 모드 -> 모드 해제
+            print("{0} : 클로킹 코드 해제합니다.".format(self.name))
+            self.clocked = False
+        else: #클로킹 모드 해제 -> 모드 설정
+            print("{0} : 클로킹 코드 설정합니다.".format(self.name))
+            self.clocked = False
 
