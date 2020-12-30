8d66eb0f66f52940d3b3720719f514adee8cabf8
msksh <74917190+msksh@users.noreply.github.com>
2020-12-30 15:39:13 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -1379,4 +1379,126 @@ print(python.count("n"))
 # price_morning(6)
 # price_soldier(7)
 
-from theater_module import price_soldier as price
+# from theater_module import price_soldier as price #theater_module에서 price_soldier를 price라고 할꺼야 라고 하는거
+# price(5)
+
+
+#패키지
+# import travel.thailland
+# trip_to = travel.thailland.TaillandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VitenamPackage()
+# trip_to.detail()
+
+
+#__all__
+
+# # from random import *
+# from travel import *
+# #trip_to = vietnam.VitenamPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+
+
+#모듈 직접 실행
+# # from random import *
+#from travel import *
+# #trip_to = vietnam.VitenamPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+# import inspect
+# import random
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+
+
+# pip install
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
+
+#내장함수
+#input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다.".format(language))
+
+# dir : 어떤 객체를 넘겨줬을때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+#print(dir())
+#import random # 외장함수
+#print(dir())
+#import pickle
+#print(dir(random))
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+# name = "Jim"
+# print(dir(name))
+
+#외장함수
+
+# glob :경로내의 폴더/ 파일 목록 조회 (윈도우dir)
+# import glob
+# print(glob.glob("*.py")) #확장자가 py 인 모든파일
+
+# os  : 운영체제에서 제공하는 기본 기능
+# import os 
+# print(os.getcwd()) #현재 디렉토리
+
+# folder = "sample_dir"
+
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os.rmdir(folder)
+#     print(folder,"폴더를 삭제하였습니다.")
+
+# else:
+#     os.makedirs(folder) #폴더생성
+#     print(folder, "폴더를 생성하였습니다.")
+
+# time : 시간 관련 함수
+#import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# import datetime
+# # print("오늘 날짜는", datetime.date.today())
+# #timedelta : 두 날짜 사이의 간격
+# today = datetime.date.today() #오늘 날짜 저장
+# td = datetime.timedelta(days=100) #100일 저장
+# print("우리가 만난지 100일은", today + td) #오늘 부터 100일 후
+
+
+# Quiz)프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+
+# 조건: 모듈 파일명은 byme.py 로 작성하시오
+# (모듈 사용 예제)
+import byme
+byme.sign()
+
+# (출력 예제)
+# 이 프로그램은 성훈이에 의해 만들어졌습니다.
+# 유튜브 : http://youtube.com
+# 이메일 : fangbe@naver.com
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
