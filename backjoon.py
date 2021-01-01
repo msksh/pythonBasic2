8d96a10b9fe59a5df12a4a0d56a39f7f2b7bf0b5
msksh <74917190+msksh@users.noreply.github.com>
2021-01-01 23:54:34 +0900

    Add files via upload

--- backjoon.py
+++ backjoon.py
@@ -4,17 +4,20 @@
 # for i in a :
 #     if y > i:
 #         print(i,end=" ")
+
 # #2439
 # n = int(input())
 # for i in range(1 ,n+1):
 #     print(" "*(n-i),end="")
 #     print("*"*i)
+
 # #2338
 # n = int(input())
 # for i in range(1 ,n+1):
 #     for j in range(0,i):
 #         print("*", end="")
 #     print()
+
 # #11021
 # import sys
 # n = int(input())
@@ -22,6 +25,7 @@
 # for i in range(n):
 #     x, y = map(int, sys.stdin.readline().rstrip().split())
 #     print("Case #{0}: {1}".format(i+1,x+y))
+
 # #11022
 # import sys
 # n = int(input())
@@ -29,14 +33,17 @@
 # for i in range(n):
 #     x, y = map(int, sys.stdin.readline().rstrip().split())
 #     print("Case #{0}: {1} + {2} = {3}".format(i+1,x,y,x+y))
+
 # #2742
 # n = int(input())
 # for i in range(n, 0, -1):
 #     print(i)
+
 # #2141
 # n = int(input())
 # for i in range(1 ,n+1):
 #     print(i)
+
 # #15552
 # import sys
 # n = int(input())
@@ -44,6 +51,7 @@
 # for i in range(n):
 #     x, y = map(int, sys.stdin.readline().rstrip().split())
 #     print(x+y)
+
 # #8393
 # x = 0
 # n = int(input())
@@ -51,6 +59,7 @@
 # for i in range(1, n+1):
 #     x += i 
 # print(x)
+
 # #10950
 # n = int(input())
 
@@ -58,9 +67,34 @@
 #     x, y = map(int, input().split())
 #     print(x+y)
 
+#10952
+# while True:
+#     x , y =  map(int, input().split())
+#     print(x+y)
+#     if x==5 and y==2 :
+#             break
 
+#10951
+# while True:
+#     try:
+#         x , y =  map(int, input().split())
+#         print(x+y)
+#     except:
+#         break 
 
-
-
-
+# 1110
+# n = int(input())
+# num = n
+# count = 0
+# while True:
+#     a = num // 10
+#     b = num % 10
+#     c = (a + b) % 10
+#     num = (b * 10) + c
+#     count = count + 1
+#     if(num == n):
+#         break
+# print(count)
+          
+    
 
